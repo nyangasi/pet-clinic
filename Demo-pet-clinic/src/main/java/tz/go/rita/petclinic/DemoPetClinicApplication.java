15b3b31f0e45a8e9712333e8aa470399127b3542
Gudfella <Gudfella@HomeOfficeDT01>
2020-03-22 11:53:58 +0300

    First Commit - Project Start

---
+++ Demo-pet-clinic/src/main/java/tz/go/rita/petclinic/DemoPetClinicApplication.java
@@ -0,0 +1,13 @@
+package tz.go.rita.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPetClinicApplication.class, args);
+	}
+
+}
---
+++ Demo-pet-clinic/src/test/java/tz/go/rita/petclinic/DemoPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package tz.go.rita.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
